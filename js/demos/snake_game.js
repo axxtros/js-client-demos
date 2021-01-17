6577fe70f7db0fc57a02ce6f8c8375669672aafd
axtros <axtros@gmail.com>
2021-01-17 14:14:10 +0100

    Snake game 2d. v.0.8

--- js/demos/snake_game.js
+++ js/demos/snake_game.js
@@ -53,16 +53,13 @@ var snakeGame = (function() {
         deletedElement: tileObject
     };
 
-    var mapObject = {
-        rowNum: 0,
-        columnNum: 0,
-        mapMatrix: new Array()
-    }
-
-    var gameMap;
+    var gameMapRow;
+    var gameMapColumn;
     var target;
     var snake;
-    var isRunning = false;
+    var isGameRunning = false;
+    var isKeyDown = true;           //a render előtt ne lehessen dupla billentyűt használni, ez nélkül a snake-et vissza lehetne fordítani saját magába (a addEventListener független a gameLoop-tól)
+    var gameSpeed = 40;
 
     //game loop variables
     var now; 
@@ -79,11 +76,19 @@ var snakeGame = (function() {
         if(gameBackgroundCanvasElement !== null && gameSnakeCanvasElement !== null && dataBackgroundCanvasElement !== null && dataDisplayCanvasElement !== null) {            
 
             //az egyes canvas-ek pontos mérete a tile méret alapján lesz kalkulálva úgy, hogy annyi tile legyen kirajzolva, amennyi elfér a böngésző aktuális méretében
-            let canvaswrapperDivWidht = document.getElementById("game-canvas-wrapper").offsetWidth;
-            let canvaswrapperDivHeight = document.getElementById("game-canvas-wrapper").offsetHeight;
+            let canvaswrapperDivWidht = Math.floor(document.getElementById("game-canvas-wrapper").offsetWidth);
+            let canvaswrapperDivHeight = Math.floor(document.getElementById("game-canvas-wrapper").offsetHeight);
 
-            let tileCanvasWidth = (Math.floor(/*window.innerWidth*/ canvaswrapperDivWidht / constans.TILESIZE) * constans.TILESIZE); // - (2 * constans.TILESIZE);
-            let tileCanvasHeight = (Math.floor(window.innerHeight / constans.TILESIZE) * constans.TILESIZE) - (2 * (constans.TILESIZE * constans.TILESIZE));
+            let tileCanvasWidth = (Math.floor(/*window.innerWidth*/ canvaswrapperDivWidht / constans.TILESIZE) * constans.TILESIZE);
+            //a game canvas-en kívüli terület nagysága, amelyet le kell vonni a teljes oldal magasságából
+            //FIGYELEM: Ez egy közelítő szám, körübelüli kalkuláció, amely változhat az oldal CSS változásai során, vagy a constans.TILESIZE változása miatt.
+            //oldal összetevők, amelyeket le kell vonni a magasságból:
+            //(header + footer [azonos magasságúak, ez a kétszeres szorzó])
+            //a dataDisplayCanvas magassága
+            //a div-ek közötti padding-ok összege + ráhagyás
+            let outerSnakeCanvasHeights = ((2 * (Math.floor(30 / constans.TILESIZE))) * constans.TILESIZE) + 
+                    (Math.floor(constans.DATA_CANVAS_HEIGHT / constans.TILESIZE) * constans.TILESIZE) + constans.WEB_PAGE_DIV_PADDING_HEIGHTS;
+            let tileCanvasHeight = (Math.floor(window.innerHeight / constans.TILESIZE) * constans.TILESIZE) - ((Math.floor(outerSnakeCanvasHeights / constans.TILESIZE) * constans.TILESIZE));
 
             //fentről lefelé kell az egyes canvas layout-okat inicializálni, és kalkulálni a pontos pozícoikat (minden canvas-t méretezni kell, hogy a ráépített grafika méretarányos maradjon!)
             util.resizeCanvas(dataBackgroundCanvasElement, tileCanvasWidth, constans.DATA_CANVAS_HEIGHT);
@@ -94,27 +99,14 @@ var snakeGame = (function() {
             util.resizeCanvas(gameSnakeCanvasElement, tileCanvasWidth, tileCanvasHeight);
             util.addNewCanvasLayer('game-snake-canvas', gameBackgroundCanvasElement.getBoundingClientRect());
 
-            initMapMatrix(tileCanvasHeight / constans.TILESIZE, tileCanvasWidth / constans.TILESIZE);
+            gameMapRow = (tileCanvasHeight / constans.TILESIZE);
+            gameMapColumn = (tileCanvasWidth / constans.TILESIZE);
             
             initCanvasContexts();
             initDataBackgroundCanvas();
             initGameBackgroundCanvas();
         } else {
             console.log('Canvas initialization error!');
-        }
-    }
-
-    function initMapMatrix(row, column) {
-        gameMap = Object.create(mapObject);
-        gameMap.row = row;
-        gameMap.column = column;
-        for(let i = 0; i != row; i++) {
-            gameMap.mapMatrix[i] = new Array(tileObject);
-            for(let j = 0; j != column; j++) {
-                let emptyTile = Object.create(tileObject);
-                emptyTile.row = emptyTile.column = 0;                
-                gameMap.mapMatrix[i][j] = emptyTile;
-            }
         }
     }
 
@@ -149,12 +141,12 @@ var snakeGame = (function() {
         createTarget();
         //https://www.kirupa.com/canvas/moving_shapes_canvas_keyboard.htm
         window.addEventListener("keydown", processInput, false);    //a billentyűzet kezelést nem kell a gameLoop-ban figyelni, ezt minden böngész támogatja
-        isRunning = true;
+        isGameRunning = true;
     }
 
     function initSnake() {
-        let snakeStartRow = math.generateRandomNumberWithLimits(0, gameMap.row - 1);
-        let snaketStartColumn = math.generateRandomNumberWithLimits(0, gameMap.column - 1);
+        let snakeStartRow = math.generateRandomNumberWithLimits(0, gameMapRow - 1);
+        let snaketStartColumn = math.generateRandomNumberWithLimits(0, gameMapColumn - 1);
         snake = Object.create(snakeObject);
         snake.head = Object.create(tileObject);
         snake.head.row = snakeStartRow;
@@ -172,9 +164,9 @@ var snakeGame = (function() {
     function createTarget() {
         let isCreateNew = false;        
         do {
-            let targetStartRow = math.generateRandomNumberWithLimits(0, gameMap.row - 1);
-            let targetStartColumn = math.generateRandomNumberWithLimits(0, gameMap.column - 1);
-            if(gameMap.mapMatrix[targetStartRow][targetStartColumn].row == 0 && gameMap.mapMatrix[targetStartRow][targetStartColumn].column == 0) {
+            let targetStartRow = math.generateRandomNumberWithLimits(0, gameMapRow - 1);
+            let targetStartColumn = math.generateRandomNumberWithLimits(0, gameMapColumn - 1);
+            if(checkEmptyTile(targetStartRow, targetStartColumn)) {
                 target = Object.create(targetObject);
                 target.pos = Object.create(tileObject);
                 target.pos.row = targetStartRow;
@@ -185,28 +177,42 @@ var snakeGame = (function() {
         } while(!isCreateNew);
     }
 
+    function checkEmptyTile(targetStartRow, targetStartColumn) {
+        if(snake.bodyTiles.length !== 0) {
+            for(let i = 0; i != snake.bodyTiles.length; i++) {
+                if(snake.bodyTiles[i].row == targetStartRow && snake.bodyTiles[i].column == targetStartColumn) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     function processInput(keyEvent) {
-        switch(keyEvent.keyCode) {
-            case 37:    //left                
-                if(snake.direction !== SNAKE_DIRECTION.RIGHT) {
-                    snake.direction = SNAKE_DIRECTION.LEFT;
-                }   
-            break;
-            case 38:    //up                
-                if(snake.direction !== SNAKE_DIRECTION.DOWN) {
-                    snake.direction = SNAKE_DIRECTION.UP;
-                }                
-            break;
-            case 39:    //right                
-                if(snake.direction !== SNAKE_DIRECTION.LEFT) {
-                    snake.direction = SNAKE_DIRECTION.RIGHT;
-                }                
-            break;
-            case 40:    //down                
-                if(snake.direction !== SNAKE_DIRECTION.UP) {
-                    snake.direction = SNAKE_DIRECTION.DOWN;
-                }                
-            break;
+        if(isKeyDown) {
+            switch(keyEvent.keyCode) {
+                case 37:    //left                
+                    if(snake.direction !== SNAKE_DIRECTION.RIGHT) {
+                        snake.direction = SNAKE_DIRECTION.LEFT;
+                    }   
+                break;
+                case 38:    //up                
+                    if(snake.direction !== SNAKE_DIRECTION.DOWN) {
+                        snake.direction = SNAKE_DIRECTION.UP;
+                    }                
+                break;
+                case 39:    //right                
+                    if(snake.direction !== SNAKE_DIRECTION.LEFT) {
+                        snake.direction = SNAKE_DIRECTION.RIGHT;
+                    }                
+                break;
+                case 40:    //down                
+                    if(snake.direction !== SNAKE_DIRECTION.UP) {
+                        snake.direction = SNAKE_DIRECTION.DOWN;
+                    }                
+                break;
+            }
+            isKeyDown = false;
         }
     }
 
@@ -215,7 +221,7 @@ var snakeGame = (function() {
         let newSneakBodyElement = Object.create(tileObject);        
         newSneakBodyElement.row = snake.head.row;
         newSneakBodyElement.column = snake.head.column; 
-        snake.bodyTiles.unshift(newSneakBodyElement);        //add first element to snakebody        
+        snake.bodyTiles.unshift(newSneakBodyElement);        //add head snake element to snakebody first place       
         snake.deletedElement = null;
         if(snake.bodyTiles.length > snake.currentLength) {
             snake.deletedElement = Object.create(tileObject);
@@ -235,8 +241,9 @@ var snakeGame = (function() {
         }
     }
 
-    function render() {
-        console.log('snake move snake.head.column: ' + snake.head.column + ' snake.head.row: ' + snake.head.row);        
+    //minden esetben csak a 'minimális' renderelés történik meg, ha valaminek nem változott a grafikus property-je, vagy a pozicíója, akkor nem rajzoljuk ki újra
+    function render() { 
+        //csak a snake head-et rajzoljuk ki, a body-ból csak az utolsó elem került törlésre  
         draw.drawFillSquare(gameSnakeContext, snake.head.column * constans.TILESIZE, snake.head.row * constans.TILESIZE, constans.TILESIZE, constans.SNAKE_TILE_GRID_COLOR);
         if(snake.deletedElement !== null) {
             draw.clearSquare(gameSnakeContext, snake.deletedElement.column * constans.TILESIZE, snake.deletedElement.row * constans.TILESIZE, constans.TILESIZE);
@@ -258,15 +265,18 @@ var snakeGame = (function() {
             //processInput();
             //update();
         }
+
         update();
         render();
+        isKeyDown = true;
+        
         last = now;
         
-        if(isRunning) {
+        if(isGameRunning) {
             setTimeout(() => { 
                 //console.log("delay...");
                 requestAnimationFrame(gameLoop); 
-            }, 100);
+            }, gameSpeed);
             //requestAnimationFrame(gameLoop);
         }
     }
@@ -284,7 +294,8 @@ var snakeGame = (function() {
 
 var constans = (function() {
 
-    var _TILESIZE = 20;        
+    var _TILESIZE = 10;
+    var _WEB_PAGE_DIV_PADDING_HEIGHTS = 60;        
     var _DATA_CANVAS_HEIGHT = 30;
     var _MAP_EMPTY_GRID = 0;
     var _CANVAS_BACKGROUND_COLOR = '#204060';
@@ -295,7 +306,8 @@ var constans = (function() {
 
     return {
 
-        TILESIZE: _TILESIZE,        
+        TILESIZE: _TILESIZE,
+        WEB_PAGE_DIV_PADDING_HEIGHTS: _WEB_PAGE_DIV_PADDING_HEIGHTS,      
         DATA_CANVAS_HEIGHT: _DATA_CANVAS_HEIGHT,
         MAP_EMPTY_GRID: _MAP_EMPTY_GRID,
         CANVAS_BACKGROUND_COLOR: _CANVAS_BACKGROUND_COLOR,
